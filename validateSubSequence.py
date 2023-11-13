4d7df13745865312d8baab3a631388dd92478557
Sugondeez Nuts <sugondeez@kabali>
2023-11-13 18:40:44 +0200

    solved validate sub-sequence

---
+++ validateSubSequence.py
@@ -0,0 +1,11 @@
+array=list(map(int, input().split()))
+sequence = list(map(int, input().split()))
+def solution(array, sequence):
+    pos = 0
+    for i in array:
+        if i == sequence[pos]:
+            pos+=1
+        if pos == len(sequence):
+            return True
+    return False        
+print(solution(array, seqquence))

2f60ac645a2a180cad19192bc2b1f6c0b345aedf
dinokage <yerradinesh01@gmail.com>
2023-11-15 08:02:19 +0200

    optimized three sum

--- threeNumberSum.py
+++ threeNumberSum.py
@@ -10,4 +10,22 @@ def solution(array, targetSum):
                 if array[a]+array[b]+array[c]==targetSum:
                     res.append([array[a],array[b],array[c]])
     return res
+def optimized_solution(array, targetSum):
+    array.sort()
+    res=[]
+    for current in range(len(array)-2):
+        left=current+1
+        right=len(array)-1
+        while left<right:
+            val=array[current]+array[left]+array[right]
+            if val==targetSum:
+                res.append([array[current], array[left], array[right]])
+                left+=1
+                right-=1
+            elif val<targetSum:
+                left+=1
+            else:
+                right-=1
+    return res
 print(solution(inputArray, target))
+print(optimized_solution(inputArray, target))

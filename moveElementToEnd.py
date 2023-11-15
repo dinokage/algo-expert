1d85e9e9227c10512875e5f279bff54bdfca42ea
dinokage <yerradinesh01@gmail.com>
2023-11-15 10:18:22 +0200

    optimized move element to end (no sort)

--- moveElementToEnd.py
+++ moveElementToEnd.py
@@ -3,6 +3,19 @@ def inputArray():
 def solution(array, toMove):
     array.sort(key = lambda x: x==toMove)
     return array
+def optimized_solution(array, toMove):
+    begin=0
+    end=len(array)-1
+    while begin<end:
+        while array[begin]!=toMove and begin<end:
+            begin+=1
+        while array[end]==toMove and begin<end:
+            end -=1
+        array[begin], array[end] = array[end], array[begin]
+        begin+=1
+        end-=1
+    return array
 array=inputArray()
 toMove = int(input())
 print(solution(array, toMove))
+print(optimized_solution(array, toMove))
